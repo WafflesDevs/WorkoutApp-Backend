4cbad03b9eb22e2f119028280f263c9fbaf23fa8
Ayaan <ayaanwaki@gmail.com>
2026-06-26 23:14:22 -0400

    Added CorsMiddleare(What Websites are allowed to use

--- app/main.py
+++ app/main.py
@@ -12,6 +12,20 @@ app.include_router(workouts.router)
 app.include_router(progress.router)
 app.include_router(admin.router)
 
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"Welcome!","To the workout app "}
